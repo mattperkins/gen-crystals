d68fc83a1878e977717647826464cfa925cf6d45
mattperkins <github@mattperkins.com>
2020-02-02 23:24:19 +0000

    configure design rule and initial shapes

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
+// GENERATIVE SYSTEM || RULE BASED DECISION MAKING SYSTEM
 const CRYSTAL_SIZE = 500
 const SIDES = 6
+let PALETTE = []
 
 function setup() {
-  createCanvas(500,500, SVG)
+  createCanvas(530,530, SVG)
+  
+  PALETTE = [ 
+    color(255,52,154), // pink 
+    color(4,0,152) // blue
+  ]
+
   noLoop()
   angleMode(DEGREES)
   rectMode(CENTER)
 }
 
 function draw() {
-  fill(0)
+  testLines() 
+}
+
+function testLines(){
+  const rando = random(1)
+  let numShapes
+  // configure rule
+  if(rando > 0.5){
+    numShapes = SIDES
+  } else{
+    numShapes = SIDES * 2
+  }
+
+  const rando2 = floor(random(0, PALETTE.length))
+  const strokeColor = PALETTE[rando2]
+
+  noFill()
+  stroke(PALETTE[0])
   push()
     translate(width/2, height/2)
-    rotate(45)
-    rect(0,0,25,25)
+    ellipse(0,0,CRYSTAL_SIZE,CRYSTAL_SIZE)
+
+    stroke(strokeColor)
+    const angle = 360 / numShapes
+    for(let i = 0; i < numShapes; i++){
+      line(0,0,0, CRYSTAL_SIZE/2)
+      rotate(angle)
+    }
   pop()
 }
-
-
-// reset the background to black and restart the sequence
-function mousePressed() {
-  background(0)
-}
-
-//fill()
-//nostroke()
-//stroke()
-//strokeWeight()
-//line()
-//rect()
-//ellipse()
-//point()

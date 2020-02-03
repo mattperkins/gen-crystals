e6aad1c899ccb0b8ee7cabfbb63e1eab789951ac
mattperkins <github@mattperkins.com>
2020-02-03 00:19:55 +0000

    refactor helpers

--- index.js
+++ index.js
@@ -21,22 +21,6 @@ function draw() {
   outlineShape()
 }
 
-function hexagon(posX,posY,radius){
-  const rotAngle = 360 / 6
-  beginShape()
-  for(let i = 0; i < 6; i++){
-    const thisVertex = pointOnCircle(posX,posY,radius, i * rotAngle)
-    vertex(thisVertex.x, thisVertex.y)
-  }
-  endShape(CLOSE)
-}
-
-function pointOnCircle(posX,posY,radius,angle){
-  const x = posX + radius * cos(angle)
-  const y = posY + radius * sin(angle)
-  return createVector(x,y)
-}
-
 function outlineShape(){
   const strokeColor = getRandomFromPalette()
   const weight = randomSelectTwo() ? 1 : 3

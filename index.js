b8d006d94ab5949cecfd1cca486bd85a6d66f2a4
mattperkins <github@mattperkins.com>
2020-02-03 00:55:52 +0000

    configure circles

--- index.js
+++ index.js
@@ -17,9 +17,29 @@ function setup() {
 }
 
 function draw() {
+  circles()
   // simpleLines()
   // outlineShape()
   testLines() 
+}
+
+function circles(){
+  const numShapes = SIDES
+  const angle = 360 / numShapes
+  const shapeSize = (CRYSTAL_SIZE/2) * 0.93
+  const position = (CRYSTAL_SIZE/2) - (shapeSize/2)
+  const strokeColor = getRandomFromPalette()
+
+  stroke(strokeColor)
+  strokeWeight(1)
+  push()
+    translate(width/2, height/2)
+    
+    for(let i = 0; i <= numShapes; i++){
+      ellipse(position, 0, shapeSize, shapeSize)
+      rotate(angle)
+    }
+  pop()
 }
 
 function simpleLines(){

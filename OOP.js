59b79f0e7ae996259c926a46a46a41ff0de8cef7
mattperkins <github@mattperkins.com>
2020-02-07 01:38:07 +0000

    add new shape

--- OOP.js
+++ OOP.js
@@ -188,3 +188,36 @@ class SteppedHexagon extends Layer{
     pop()
   }
 }
+
+// *** -> new shape 3.2.20 <- ***
+class Triangler extends Layer{
+  constructor(){
+    super()
+    this.steps = floor(random(1, this.stepsOut))
+    this.center = this.steps * this.singleStep
+    this.randomShape = random(1)
+    this.direction = randomSelectTwo() // used for Triangle only
+    this.fillColor = randomSelectTwo() ? this.layerColor : color(0,1)
+    this.weight = randomSelectTwo() ? this.thinStroke : this.thickStroke
+
+    // control overlap
+    if(this.steps < this.stepsOut / 2){
+      this.radius = floor(random(1, this.steps)) * this.singleStep
+    } else if(this.steps > this.stepsOut / 2){
+      this.radius = floor(random(1, this.stepsOut - this.steps)) * this.singleStep
+    } else{
+      this.radius = floor(random(1, (this.stepsOut / 2) + 1)) * this.singleStep
+    }
+  }
+  render(){
+    stroke(this.layerColor)
+    fill(this.fillColor)
+    strokeWeight(this.weight)
+    push()
+    for(let i=0; i<this.numShapes; i++){  
+      myTriangle(this.center, this.radius, this.center)
+      rotate(this.angle)
+    }
+    pop()
+  }
+}

249e0b7a0aaf9036f7587aed7779bcac673d4cf6
mattperkins <github@mattperkins.com>
2020-02-07 01:38:30 +0000

    add new shape

--- dials.js
+++ dials.js
@@ -1,27 +1,29 @@
 const 
   SIDES                      = 6      // 6 -> TBD: add max @50
   LAYER_ANGLE                = 360    // 360
-  STEPSOUT                   = 4      // 8 -> TBD: add max @16
+  STEPSOUT                   = 16      // 8 -> TBD: add max @16
   THINSTROKE                 = .1      // 1
   THICKSTROKE                = 1      // 3 -> TBD: add max @20
 
   SIMPLELINES_ANGLE          = 360    // 360
   SIMPLELINES_STEPSOUT_MULTI = 0.25   // 1.25
-  SIMPLELINES_SIDES_MULTI    = 2     // 2 (max = 20)
+  SIMPLELINES_SIDES_MULTI    = 20     // 2 (max = 20)
   DOTTEDLINES_ANGLE          = 360    // 360
   DOTTEDLINES_SIDES_MULTI    = 5     // 2 (max = 10)
   DOTTEDLINES_SHAPESIZE      = 3      // 3 (max = 40)
-  STEPPEDHEX_STEPSOUT_MULTI  = 3      // 1.25 (max = 5)
+  STEPPEDHEX_STEPSOUT_MULTI  = 1.5      // 1.25 (max = 5)
 
-  CIRCLES_SHAPESIZE_MULTI    = 0.8    // 0.93 (max = 2.1)
-  OUTLINESHAPE_HEXDIVIDE     = 6      // 6 (min = 2)
+  CIRCLES_SHAPESIZE_MULTI    = 1.1    // 0.93 (max = 2.1)
+  OUTLINESHAPE_HEXDIVIDE     = 3      // 6 (min = 2)
   STEPPEDHEX_CENTEROFFSET    = 0.15   // 0.15 (max = 1) 
 
-                                      // 1 = DONT DISPLAY SHAPE
-  OUTLINESHAPE_WEIGHT        = 0.6    // 0.3
-  SIMPLELINES_WEIGHT         = 0.2    // 0.3
-  CIRCLES_WEIGHT             = 0.9    // 0.3
-  DOTTEDLINES_WEIGHT         = 0.3    // 0.3
-  CENTEREDSHAPE_WEIGHT       = 0.8    // 0.3
-  RINGOFSHAPES_WEIGHT        = 0.5    // 0.3
-  STEPPEDHEXAGON_WEIGHT      = 0.7    // 0.3
+                             // 1 = MUTE / DONT DISPLAY SHAPE
+  OUTLINESHAPE_WEIGHT        = 0.3    
+  SIMPLELINES_WEIGHT         = 0.4    
+  CIRCLES_WEIGHT             = 0.5    
+  DOTTEDLINES_WEIGHT         = 0.3    
+  CENTEREDSHAPE_WEIGHT       = 0.1    
+  RINGOFSHAPES_WEIGHT        = 0.6    
+  STEPPEDHEXAGON_WEIGHT      = 0.8    
+  TRIANGLER_WEIGHT           = 0.9
+  TRIANGLER2_WEIGHT          = 0.2
